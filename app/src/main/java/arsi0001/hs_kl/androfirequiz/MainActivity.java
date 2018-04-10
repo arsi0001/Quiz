9218b73d5f93b1c40ae8996c06960ee82c8e2f83
arsi0001 <1.arthur.simon@gmail.com>
2018-04-10 23:22:00 +0200

    Sliderviewer angefangen

--- app/src/main/java/arsi0001/hs_kl/androfirequiz/MainActivity.java
+++ app/src/main/java/arsi0001/hs_kl/androfirequiz/MainActivity.java
@@ -1,16 +1,25 @@
 package arsi0001.hs_kl.androfirequiz;
 
+import android.content.DialogInterface;
+import android.os.UserHandle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import arsi0001.hs_kl.androfirequiz.Model.Nutzer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,27 +40,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Firebase
         _database = FirebaseDatabase.getInstance();
         _alleNutzer = _database.getReference("Nutzer");
 
+        //Textfelder
         _edTNutzer = (MaterialEditText) findViewById(R.id.edTNutzer);
         _edTPasswort = (MaterialEditText) findViewById(R.id.edTPasswort);
 
+        //Buttons
         _btnAnmelden = (Button) findViewById(R.id.btn_anmelden);
         _btnRegistrieren = (Button) findViewById(R.id.btn_registrieren);
 
+        //Listener für Buttons
         _btnRegistrieren.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                zeigeRegistrationsDialog();
+                registiereNutzer();
+            }
+        });
+        _btnAnmelden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nutzerAnmelden(_edTNutzer.getText().toString(), _edTPasswort.getText().toString());
+            }
+        });
+
+
+    }
+
+    //Anmeldung des Nutzers
+    private void nutzerAnmelden(final String nutzer, final String passwort) {
+        _alleNutzer.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Pruefe ob Nutzer vorhanden -> Pruefe Passwort
+                if(dataSnapshot.child(nutzer).exists()){
+                    if(!nutzer.isEmpty()){
+                        Nutzer login = dataSnapshot.child(nutzer).getValue(Nutzer.class);
+                        if(login.get_passwort().equals(passwort))
+                            Toast.makeText(MainActivity.this, "Login erfolgreich!", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Falsches Passwort!", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Nutzername fehlt!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                //Pruefe ob Nutzer vorhanden -> Account nicht vorhanden
+                else
+                    Toast.makeText(MainActivity.this, "Nutzer existiert nicht!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
 
-    private void zeigeRegistrationsDialog() {
-        //Feedback
+    //Registration eines neuen Nutzers
+    private void registiereNutzer() {
+        //Ueberschrift bei Registration
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-        alertDialog.setTitle("Registriert");
+        alertDialog.setTitle("Registration");
         alertDialog.setMessage("Bitte vollständige Angaben machen!");
 
         LayoutInflater inflater = this.getLayoutInflater();
@@ -61,5 +114,45 @@ public class MainActivity extends AppCompatActivity {
         _edTNeueEmail = (MaterialEditText) registierungs_layout.findViewById(R.id.edTNeueEmail);
         _edTNeuesPasswort = (MaterialEditText) registierungs_layout.findViewById(R.id.edTNeuesPasswort);
 
+        alertDialog.setView(registierungs_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+
+        //Abbruch der Registration
+        alertDialog.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        //Bestaetigung der Registration
+        alertDialog.setPositiveButton("Registrieren", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final Nutzer nutzer = new Nutzer(_edTNeuerNutzer.getText().toString(),
+                        _edTNeuesPasswort.getText().toString(),
+                        _edTNeueEmail.getText().toString());
+
+                 _alleNutzer.addListenerForSingleValueEvent(new ValueEventListener() {
+                     @Override
+                     public void onDataChange(DataSnapshot dataSnapshot) {
+                         if(dataSnapshot.child(nutzer.get_nutzername()).exists())
+                             Toast.makeText(MainActivity.this, "Nutzer existiert bereits!", Toast.LENGTH_SHORT).show();
+                         else
+                         {
+                             _alleNutzer.child(nutzer.get_nutzername()).setValue(nutzer);
+                             Toast.makeText(MainActivity.this, "Registration erfolgreich!", Toast.LENGTH_SHORT).show();
+                         }
+                     }
+
+                     @Override
+                     public void onCancelled(DatabaseError databaseError) {
+
+                     }
+                 });
+                 dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
---
+++ app/src/main/java/arsi0001/hs_kl/androfirequiz/Startseite.java
@@ -0,0 +1,13 @@
+package arsi0001.hs_kl.androfirequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Startseite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startseite);
+    }
+}
